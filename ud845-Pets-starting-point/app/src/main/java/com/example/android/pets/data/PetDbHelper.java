d10bf5403530f23a4d144702e0c95fdc44ba98df
tealstar <mary03302004@gmail.com>
2018-10-23 12:39:30 -0700

    Initial commit

---
+++ ud845-Pets-starting-point/app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,46 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.android.pets.data.PetContract.PetEntry;
+
+public class PetDbHelper extends SQLiteOpenHelper {
+
+        //name database file
+        private static final String DATABASE_NAME = "shelter.db";
+
+        //database version
+        private static final int DATABASE_VERSION = 1;
+
+        //CONSTRUCTS A NEW INSTANCE OF PETDBHELPER
+        //@param context of the app
+
+        public PetDbHelper(Context context){
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        //This is called when the database is created for the first time.
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Create a String that contains the SQL statement to create the pets table
+        String SQL_CREATE_PETS_TABLE =  "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
+                + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
+                + PetEntry.COLUMN_PET_BREED + " TEXT NOT NULL, "
+                + PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, "
+                + PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+
+        // Execute the SQL statement
+        db.execSQL(SQL_CREATE_PETS_TABLE);
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
+
